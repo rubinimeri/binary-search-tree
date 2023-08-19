8e9c9d3b49c7d9fea59a6a97f4bb9ecb59a0bbed
Rubin Imeri <rubinimeri6@gmail.com>
2023-08-19 13:17:08 +0200

    Create class 'Tree' and add buildTree method
    buildTree accepts an array, sorts the array and removes
    any duplicates within it

--- balanced-bst.js
+++ balanced-bst.js
@@ -5,3 +5,17 @@ class Node {
         this.right = null
     }
 }
+
+class Tree {
+    constructor () {
+        this.root = null
+    }
+
+    buildTree (arr) {
+        arr.sort((a, b) => a - b);
+        return [...new Set(arr)];
+    }
+}
+const arr = [4, 3, 2, 1, 1, 2, 4, 5, 5]
+const tree = new Tree();
+console.log(tree.buildTree(arr))
